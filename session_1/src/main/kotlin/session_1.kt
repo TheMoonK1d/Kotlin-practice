38716f98fb6bec8cc5a8739c7ab4db6a3af5cd11
Eyob <eyobdesta562@gmail.com>
2023-04-09 06:51:46 +0300

    Session 2

--- session_1/src/main/kotlin/session_1.kt
+++ session_1/src/main/kotlin/session_1.kt
@@ -1,5 +1,28 @@
 fun main (){
-    println("Hello world")
+//    println("Hello world")
+//    var someNumber: Int = 5
+//    println(someNumber)
+//    //
+//    var anotherNumber = 5
+//    print(anotherNumber)
+//    //
+//
+//    var num = true
+
+    //HomeWork
+    var favNum = 562
+    var age = 20
+
+    println("I am $age years old, and my favorite number is $favNum")
+
+    //HomeWork2
+    var x = 3
+    var y = 4
+    var z = 5
+
+    println(x)
+    println(x+y)
+    println(x+y+z)
 
 
 }
